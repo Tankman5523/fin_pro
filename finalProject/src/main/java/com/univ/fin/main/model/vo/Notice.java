217828035575b2b541d451032ddadcc5a305907f
suji <ssosso8877@gmail.com>
2023-06-12 18:15:38 +0900

    공지게시판 페이징처리

---
+++ finalProject/src/main/java/com/univ/fin/main/model/vo/Notice.java
@@ -0,0 +1,23 @@
+package com.univ.fin.main.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notice {
+	private int noticeNo;//	NOTICE_NO	NUMBER
+	private String professorNo;//	PROFESSOR_NO	VARCHAR2(30 BYTE)
+	private String noticeCategory;//	NOTICE_CATEGORY	NUMBER
+	private String noticeTitle;//	NOTICE_TITLE	VARCHAR2(100 BYTE)
+	private String noticeContent;//	NOTICE_CONTENT	VARCHAR2(600 BYTE)
+	private int count;//	COUNT	NUMBER
+	private String createDate;//	CREATE_DATE	DATE
+	private String filed;//	FIELD	VARCHAR2(2 BYTE)
+	private String status;//	STATUS	VARCHAR2(2 BYTE)
+	private String originName;
+}
