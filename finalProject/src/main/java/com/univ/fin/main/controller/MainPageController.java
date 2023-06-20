d428fd8a1d46156c94bad8e787db17ceedfa46a8
suji <ssosso8877@gmail.com>
2023-06-20 18:20:14 +0900

    상담 교수

--- finalProject/src/main/java/com/univ/fin/main/controller/MainPageController.java
+++ finalProject/src/main/java/com/univ/fin/main/controller/MainPageController.java
@@ -167,6 +167,7 @@ public class MainPageController {
 		
 		model.addAttribute("slist", list);
 		model.addAttribute("searchPi", pi);
+		model.addAttribute("keyword", keyword);
 		
 		return "main/searchNoticeList";
 	}
--- finalProject/src/main/java/com/univ/fin/member/controller/ProfessorController.java
+++ finalProject/src/main/java/com/univ/fin/member/controller/ProfessorController.java
@@ -228,5 +228,13 @@ public class ProfessorController {
 			return (result>0)? "Y": "N";
 		}
 	}
+	
+	@RequestMapping("counselHistory.pr")
+	public String counselHistory() {
+		
+		return "member/professor/counselHistory";
+	}
+	
+	
 
 }
